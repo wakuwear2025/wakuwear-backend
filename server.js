64c84e348552bcfed25aef58959ec7832fc0265f
Praful Sharma <wakuwear2025@gmail.com>
2025-12-24 04:53:52 +0530

    Register products route

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const productRoutes = require('./routes/productRoutes');
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/products', productRoutes);
+
 
 mongoose.connect(process.env.MONGO_URI, {
   authSource: 'admin',
