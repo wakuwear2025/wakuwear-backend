98fc0d8e89564cd725916ffc97e7ad31650c6625
Praful Sharma <wakuwear2025@gmail.com>
2025-12-24 03:25:47 +0530

    Add app.listen to start server

--- server.js
+++ server.js
@@ -18,4 +18,13 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch((err) => {
   console.error('MongoDB connection error:', err.message);
 });
+app.get('/', (req, res) => {
+  res.send('Wakuwear API running');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
